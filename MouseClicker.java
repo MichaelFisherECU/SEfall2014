b7c8fea4b6289e70b0b5cdbedf472218aafadfa9
MichaelFisherECU <fisherm10@students.ecu.edu>
2014-12-09 11:48:56 -0500

    Update MouseClicker.java
    Inserted comments and cleaned up MouseClicker.java

--- MouseClicker.java
+++ MouseClicker.java
@@ -4,16 +4,22 @@ import java.awt.Robot;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+/*
+ * Implements MouseListener for recording mouse input. 
+ * The only implemented method used is the MouseClicked, 
+ * therefore only stationary input is recorded.
+ */
+
 public class MouseClicker implements MouseListener{
 
-	static Boolean drawMe = true;
+	static Boolean draw = true;
 	private static Boolean drawRay = false;
 	static Boolean rayCastTime = false;
 
-	//Gets the location of the mouse click
+	// mouseClicked(arg0) gets the location of the mouse click at arg0
 	@Override
-	public void mouseClicked(MouseEvent arg0){
-	
+	public void mouseClicked(MouseEvent arg0)
+	{
 		if(rayCastTime)//if the polygon has been drawn.
 		{
 			PolygonArea.rayCast(arg0.getX(), arg0.getY());
@@ -24,7 +30,6 @@ public class MouseClicker implements MouseListener{
 		{
 			Interface.pa.addtoPoly(arg0.getX(), arg0.getY());
 		}
-		
 	}
 	@Override
 	public void mouseEntered(MouseEvent e) {
@@ -46,20 +51,36 @@ public class MouseClicker implements MouseListener{
 		// TODO Auto-generated method stub
 		
 	}
-	static void drawTime(boolean b) {
-		drawMe = b;
+	
+	
+	// drawTime(b) sets draw to b
+	
+	static void drawTime(boolean b) 
+	{
+		draw = b;
 	}
+	
+	
+	// getDrawRay() returns the status of the ray cast
+	
 	public static Boolean getDrawRay() {
 		return drawRay;
 	}
+	
+	
+	// setDrawRay(b) sets drawRay to b
 	
 	public static void setDrawRay(Boolean b)
 	{
 		drawRay = b;
 	}
-	public static void setRayCastTime(boolean b) {
+	
+	
+	// setRayCastTime(b) sets rayCastTime to b
+	
+	public static void setRayCastTime(boolean b) 
+	{
 		rayCastTime = b;
-		
 	}
-};
+}
 	
